5b9ea9a0a38bbadabce03fef64904cff0ba5a60f
Cody Robertson <cody@hawkhost.com>
2011-05-12 19:48:11 -0400

    Added basic nslookup plugin, added README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Wobot Plugins
+
+This is a repository of Wobot (http://github.com/cjoudrey/wobot) plugins we use
+at Hawk Host on HipChat (http://hipchat.com).
+
+# nslookup.js
+
+A very simple plugin that attempts to resolve a given hostname
+
+	Cody: !nslookup hawkhost.com
+	Hawk Bot: @Cody DNS Lookup: 208.43.49.45
---
+++ nslookup.js
@@ -0,0 +1,35 @@
+// This is a sample plugin that will display
+// the temperature for a given zip code when
+// someone types !weather ZIPCODE in a channel.
+
+var dns = require('dns');
+
+module.exports.load = function(bot) {
+  bot.on('message', onMessage);
+};
+
+module.exports.unload = function(bot) {
+  bot.removeListener('message', onMessage);
+};
+
+var onMessage = function(channel, from, message) {
+  var matches = message.match(/^\!nslookup (.*)$/i);
+  var ret = "";
+  var self = this;
+  if (!matches) return false;
+
+  dns.resolve4(matches[1], function(err, addresses) {
+	if (err) {
+		self.message(channel, '@' + from.split(' ')[0] + ' ' + 'Error with lookup!');
+		return true;
+	}
+
+	addresses.forEach(function (address) {
+		ret += address + " ";
+	});
+
+	self.message(channel, '@' + from.split(' ')[0] + ' DNS Lookup: ' + ret);
+  });
+
+  return true;
+};
